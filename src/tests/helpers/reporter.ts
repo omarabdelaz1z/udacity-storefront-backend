da5c3d6a0edcb82b292dbfffd4ea73500de5ac9a
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-02-24 23:50:44 +0200

    feat: jasmine init

---
+++ src/tests/helpers/reporter.ts
@@ -0,0 +1,31 @@
+import {
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption,
+} from "jasmine-spec-reporter";
+
+import JasmineStartedInfo = jasmine.JasmineStartedInfo;
+/**
+ * 
+ */
+class CustomProcessor extends DisplayProcessor {
+/**
+ * Display Log
+ * @param {JasmineStartedInfo} _ StartedInfo.
+ * @param {string} log spec string.
+ * @return {string} log message
+ */
+  public displayJasmineStarted(_: JasmineStartedInfo, log: string): string {
+    return `${log}`;
+  }
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(
+  new SpecReporter({
+    spec: {
+      displayStacktrace: StacktraceOption.NONE,
+    },
+    customProcessors: [CustomProcessor],
+  })
+);
