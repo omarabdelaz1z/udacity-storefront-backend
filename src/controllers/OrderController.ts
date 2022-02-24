162173a1b67f97d39efc63b1fadb0d77fa9b3710
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-02-24 23:17:57 +0200

    fix: add status as parameter to findOrderByUserId method

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { findOrdersByUserId } from "../models/Order";
+import { findOrdersByUserId, OrderStatus } from "../models/Order";
 import logger from "../utils/log/logger";
 
 export const findOrdersByUserIdHandler = async (
@@ -9,8 +9,12 @@ export const findOrdersByUserIdHandler = async (
 ) => {
   const userId = Number(req.query.userId) || Number(req.params.id);
 
+  const status: OrderStatus = req.query.status
+    ? (req.query.status as OrderStatus)
+    : undefined;
+
   try {
-    const response = await findOrdersByUserId(userId);
+    const response = await findOrdersByUserId(userId, status);
 
     if (response?.error)
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response.error);
