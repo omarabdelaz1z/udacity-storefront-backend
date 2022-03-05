1f2fa13765e3aff4e55d813504d898b9a910e715
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-03-05 23:32:12 +0200

    feat: set env variable for testing

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,16 @@
+import dotenv from "dotenv";
 import { Pool } from "pg";
 
-export default new Pool({
+dotenv.config();
+
+const pool = new Pool({
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database:
+    process.env.DB_NAME === "DEVELOPMENT"
+      ? process.env.DB_NAME
+      : process.env.DB_NAME_TEST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
+
+export default pool;
