09052698ebf63da28fd524d8e401ccc758185ddf
omarabdelaz1z <omarabdelaziz042@gmail.com>
2022-03-05 23:29:11 +0200

    limit json size to 2mb

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,9 +2,8 @@ import { Application, json } from "express";
 import cors from "cors";
 import morgan from "morgan";
 
-
 const mount = (app: Application) => {
-  app.use(json());
+  app.use(json({ limit: "2mb" }));
   app.use(cors());
 
   if (process.env.NODE_ENV === "DEVELOPMENT") app.use(morgan("dev"));
